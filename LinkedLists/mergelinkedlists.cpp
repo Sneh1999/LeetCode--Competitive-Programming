e05b1cf80184f2bd3356e8c55599b42ed3b0b8a9
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2020-04-22 13:09:15 -0400

    Update mergelinkedlists.cpp

--- LinkedLists/mergelinkedlists.cpp
+++ LinkedLists/mergelinkedlists.cpp
@@ -17,51 +17,101 @@ Output: 1->1->2->3->4->4
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    ListNode* l3;
-     while(l1 != NULL || l2 != NULL){
-        if(l1->val < l2->val){
-            l3->val = l1->val;
-            l3->next = l3->ListNode(0);
-            l3 = l3->next;
+        ListNode* head = NULL;
+        ListNode* copy = NULL;
+        
+        if(l1 == NULL and l2 == NULL){
+            return l1;
+        }
+        if(l1 == NULL and l2 != NULL){
+            return l2;
+        }
+         if(l2 == NULL and l1 != NULL){
+            return l1;
+        }
+        while(l1 != NULL and l2 != NULL){
+            
+            if(l1->val >= l2->val){
+                if(head == NULL){
+                    head = new ListNode(l2->val);
+                    copy = head;
+                    head->next = new ListNode(0);
+                    head = head->next;
+                    
+                }   else{
+                    
+                   head->val = l2->val;
+                    head->next = new ListNode(0);
+                    head = head->next;
+                    
+                    }
+                 l2 = l2->next;
+                
+            }
+            else{
+             if(head == NULL){
+                    head = new ListNode(l1->val);
+                    copy = head;
+                    head->next = new ListNode(0);
+                    head = head->next;
+                 
+                }   else{
+                    head->val = l1->val;
+                     head->next = new ListNode(0);
+                    head = head->next;
+                    
+                }
+                  l1 = l1->next;
+            }
+            
+          
            
         }
-        else if( l2->val > l1->val){
-            l3->val = l2->val;
-            l3->next = l3->ListNode(0);
-            l3 = l3->next;
+        
+        if(l1 != NULL){
+            
+            while(l1 != NULL){
+                
+                  head->val = l1->val;
+                  
+                    if(l1->next !=NULL){
+                    head->next = new ListNode(0);
+                     head = head->next;
+                }
+                    
+                 l1 = l1->next;
+            }
         }
-        else if( l2->val === l1->val){
-            l3->val = l1->val;
-            l3->next = l3->ListNode(l1->val);
-            l3->next->next = l3->ListNode(0);
-            l3 = l3->next->next;
+        
+          if(l2 != NULL){
+            
+            while(l2 != NULL){
+                head->val = l2->val;
+               
+                if(l2->next !=NULL){
+                     head->next = new ListNode(0);
+                     head = head->next;
+                }
+               
+                    
+                l2 = l2->next; 
+            }
         }
-        l1 = l1->next;
+        
+      
         
         
-        l2 = l2->next;
+        return copy;
     }
-    
-    if(l2 != NULL){
-        while(l2 != NULL){
-            l3->val = l2->val;
-            l3->next = l3->ListNode(0);
-            l3 = l3->next;
-            l2 = l2->next;
-        }
-    }
-    if(l1 != NULL){
-        while(l1 != NULL){
-             l3->val = l1->val;
-            l3->next = l3->ListNode(0);
-            l3 = l3->next;
-            l1 = l1->next;
-        }
-    }
-    
-        return l3;
-}
 };
