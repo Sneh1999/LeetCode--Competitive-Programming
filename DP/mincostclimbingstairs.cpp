6cd4594221929a7ec50633113f1ec1af6ac86cdc
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2020-05-09 10:58:02 -0400

    Update mincostclimbingstairs.cpp

---
+++ DP/mincostclimbingstairs.cpp
@@ -0,0 +1,44 @@
+/*
+746. Min Cost Climbing Stairs
+Easy
+
+1757
+
+400
+
+Add to List
+
+Share
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+
+Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+
+Example 1:
+Input: cost = [10, 15, 20]
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+Example 2:
+Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+Output: 6
+Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+
+
+*/
+
+
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        vector<int> dp(cost.size()+1,0);
+        int n = cost.size();
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i <= n; ++i) {
+           dp[i] = min(dp[i-1], dp[i-2]) + (i == n ? 0 : cost[i]);
+        }
+ 
+    return dp[n];
+    }
+};
